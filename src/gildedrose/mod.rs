d476d6ef189401dc99946479e278157a7714c100
Mathieu Chauvin <mathieu.chauvin@orange.com>
2020-01-25 10:14:12 +0100

    refacto(): switch item type

--- src/gildedrose/mod.rs
+++ src/gildedrose/mod.rs
@@ -4,12 +4,12 @@ use std::vec;
 pub struct Item {
     pub name: string::String,
     pub sell_in: i32,
-    pub quality: i32
+    pub quality: i32,
 }
 
 impl Item {
     pub fn new(name: String, sell_in: i32, quality: i32) -> Item {
-        Item {name: name, sell_in: sell_in, quality: quality}
+        Item { name: name, sell_in: sell_in, quality: quality }
     }
 }
 
@@ -19,59 +19,66 @@ pub struct GildedRose {
 
 impl GildedRose {
     pub fn new(items: vec::Vec<Item>) -> GildedRose {
-        GildedRose {items: items}
+        GildedRose { items: items }
     }
 
     pub fn update_quality(&mut self) {
         for item in &mut self.items {
-            if item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert" {
-                if item.quality > 0 {
-                    if item.name != "Sulfuras, Hand of Ragnaros" {
-                        item.quality = item.quality - 1;
-                    }
-                }
+            if item.name == "Aged Brie" {
+                GildedRose::update_brie(item)
+            } else if item.name == "Backstage passes to a TAFKAL80ETC concert" {
+                GildedRose::update_backstage(item)
+            } else if item.name == "Sulfuras, Hand of Ragnaros" {
+                GildedRose::update_sulfuras();
             } else {
-                if item.quality < 50
-                {
-                    item.quality = item.quality + 1;
-
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert" {
-                        if item.sell_in < 11 {
-                            if item.quality < 50 {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-
-                        if item.sell_in < 6 {
-                            if item.quality < 50 {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-                    }
-                }
+                GildedRose::update_normal(item)
             }
+        }
+    }
 
-            if item.name != "Sulfuras, Hand of Ragnaros" {
-                item.sell_in = item.sell_in - 1;
-            }
+    fn update_sulfuras() {
+        // nop !
+    }
 
-            if item.sell_in < 0 {
-                if item.name != "Aged Brie" {
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert" {
-                        if item.quality > 0 {
-                            if item.name != "Sulfuras, Hand of Ragnaros" {
-                                item.quality = item.quality - 1;
-                            }
-                        }
-                    } else {
-                        item.quality = item.quality - item.quality;
-                    }
-                } else {
-                    if item.quality < 50 {
-                        item.quality = item.quality + 1;
-                    }
-                }
-            }
+    fn update_brie(item: &mut Item) -> () {
+        GildedRose::increase_quality(item);
+        item.sell_in = item.sell_in - 1;
+        if item.sell_in < 0 {
+            GildedRose::increase_quality(item);
+        }
+    }
+
+    fn update_backstage(item: &mut Item) -> () {
+        GildedRose::increase_quality(item);
+        if item.sell_in < 11 {
+            GildedRose::increase_quality(item);
+        }
+        if item.sell_in < 6 {
+            GildedRose::increase_quality(item);
+        }
+        item.sell_in = item.sell_in - 1;
+        if item.sell_in < 0 {
+            item.quality = item.quality - item.quality;
+        }
+    }
+
+    fn update_normal(item: &mut Item) -> () {
+        GildedRose::decrease_quality(item);
+        item.sell_in = item.sell_in - 1;
+        if item.sell_in < 0 {
+            GildedRose::decrease_quality(item);
+        }
+    }
+
+    fn decrease_quality(item: &mut Item) {
+        if item.quality > 0 {
+            item.quality = item.quality - 1;
+        }
+    }
+
+    fn increase_quality(item: &mut Item) -> () {
+        if item.quality < 50 {
+            item.quality = item.quality + 1;
         }
     }
 }
